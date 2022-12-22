ad43c2234b6330818d6e3e0f6c5fd4ae64081ac2
BT22105087 <118710396+BT22105087@users.noreply.github.com>
2022-12-22 11:51:16 +0530

    Add files via upload

---
+++ 2 question.py
@@ -0,0 +1,5 @@
+gr=int(input("enter the gross income"))
+dp=int(input("enter the no of dependents"))
+tax=gr-10000-(3000*dp)
+taxp=tax*0.20
+print(taxp)
---
+++ 3 question.py
@@ -0,0 +1,6 @@
+#3 question
+sec=int(input("enter the seconds to convert"))
+m=sec//60
+r=sec%60
+print("no. of minutes =",m)
+print("no. of seconds=",r)
